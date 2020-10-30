98163fccbe94248cb0ad9e3724375c32d1df23f4
johnbelmdev <johnbel458@gmail.com>
2020-10-29 23:33:05 -0400

    commit

--- Repeated String/index.js
+++ Repeated String/index.js
@@ -9,7 +9,74 @@ while(temp.length<n){
  temp = temp.concat(' ',s)
 
 }
-// starting from and end at nimmddffddffd time
+// jdjfdfkjdfjkfdkfdjkfdjfjfdjkdfjkdfkjdfjk
+// starting from and end at dfdfdfddfd time
+dfdfdfddfddfdfdfddfd
+dfdfdfddfddfdfdfddfddfdfdfddfd
+dfdfdfddfddfdfdfddfddfdfdfddfd
+dfdfdfddfddfdfdfddfd
+dfdfdfddfddfdfdfddfddfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+
+
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfddfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+dfdfdfddfd
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+dfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfd
 temp =temp.substr(0,n)
 console.log(temp);
 for (var i = 0; i < temp.length; i++) {
