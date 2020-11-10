573f39af23fdaa860b25e220f588209680f457a5
johnbelmdev <johnbel458@gmail.com>
2020-11-10 17:26:24 -0500

    commit

--- Repeated String/index.js
+++ Repeated String/index.js
@@ -3,7 +3,7 @@ function repeatedstring(s,n) {
 
 let temp = s;
 //set count to zero
-let count=0;
+let count=000;
 while(temp.length<n){
 
  temp = temp.concat(' ',s)
@@ -11,72 +11,6 @@ while(temp.length<n){
 }
 // jdjfdfkjdfjkfdkfdjkfdjfjfdjkdfjkdfkjdfjk
 // starting from and end at dfdfdfddfd time
-dfdfdfddfddfdfdfddfd
-dfdfdfddfddfdfdfddfddfdfdfddfd
-dfdfdfddfddfdfdfddfddfdfdfddfd
-dfdfdfddfddfdfdfddfd
-dfdfdfddfddfdfdfddfddfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-
-
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfddfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-dfdfdfddfd
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-dfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfddfdfdfddfd
 temp =temp.substr(0,n)
 console.log(temp);
 for (var i = 0; i < temp.length; i++) {
